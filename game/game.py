f9568ae9fc281bdf8fd583950ccd778b2f64ee1a
Kevin Wu <Kevin.yuanbo@gmail.com>
2024-03-23 17:43:53 -0500

    upd

--- game/game.py
+++ game/game.py
@@ -1,5 +1,10 @@
 import time
 from collections import deque
+from board import Board
+
+M = 10
+N = 10
+players = 2
 
 class Game:
 
@@ -8,13 +13,25 @@ class Game:
 
         self.houses = 0
         self.soldiers = 0
-        self.q = deque()
+        self.q1 = deque()
+        self.q2 = deque()
 
-        # self.board = Board()
+        self.board = Board(M, N, players)
 
     
     def start(self):
         return
+    
+    # every timestep second, pop the q1 and q2 and update the board
+    def update(self):
+        while True:
+            if len(self.q1) > 0:
+                move = self.q1.popleft()
+                self.board.move(move)
+            if len(self.q2) > 0:
+                move = self.q2.popleft()
+                self.board.move(move)
+            time.sleep(self.timestep)
 
 
 if __name__ == "__main__":
